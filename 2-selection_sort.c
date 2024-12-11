4e6f5f9e663a25269cbfa1f6fe15c8ba0a0bf404
vlldnt <vieilledent.adrien@gmail.com>
2024-12-11 13:49:33 +0100

    betty style for sort.h

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,4 +8,13 @@
 void selection_sort(int *array, size_t size)
 {
 
+	size_t i, j;
+	int small, temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		if (array[i + 1] < array[i])
+			small = array[i + 1];
+		small = array[i]
+	}
 }
