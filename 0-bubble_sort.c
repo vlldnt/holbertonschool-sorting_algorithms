311d79f91eb3be7398b0d454ba1d89ac7da8411a
vlldnt <vieilledent.adrien@gmail.com>
2024-12-11 11:50:36 +0100

    Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,7 +19,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (i = 0; i < size - 1; i++)
 		{
-			if (array[i + 1] < array[i])
+			if (array[i] > array[i + 1])
 			{
 				temp = array[i + 1];
 				array[i + 1] = array[i];
